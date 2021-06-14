7318baf224183361fd6f111b1ec5a1bdb3586ef1
Cho-Hyeongrae <77897596+Cho-Hyeongrae@users.noreply.github.com>
2021-06-15 00:51:55 +0900

    Update HROS-AD.py

--- Anomaly_detection/HROS-AD.py
+++ Anomaly_detection/HROS-AD.py
@@ -45,9 +45,12 @@ outliers_fraction =  args.outliers_fraction
 ###################################
 
 class HROSAD_offline:
-
-    def HROS(self, heartrate, steps):
-
+    #필터링 단계
+    def HROS(self, heartrate, steps):   
+         # HROS은 심박수 및 걸음수 데이터를 사용하여 안정시 심박수를 추론합니다. 
+        
+        
+        #connect DB
         conn = psycopg2.connect(host="localhost", port = 5432, 
         database="postgres", user="postgres", password="7452")
         cur1 = conn.cursor()
@@ -80,26 +83,30 @@ class HROSAD_offline:
         df_steps = df_steps.set_index('datetime')
         df_steps.index.name = None
         df_steps.index = pd.to_datetime(df_steps.index)
-        df_steps["steps"] = df_steps["steps"].apply(lambda x: x + 1)
+        df_steps["steps"] = df_steps["steps"].apply(lambda x: x + 1) # 0분활 문제를 박기위해 모든 걸음 수에 +1를 합니다.
 
         # merge dataframes
-        df1 = pd.merge(df_hr, df_steps, left_index=True, right_index=True)
-        df1['heartrate'] = (df1['heartrate']/df1['steps']) 
+        df1 = pd.merge(df_hr, df_steps, left_index=True, right_index=True) #같은 날,시간,분,초 심박수와 걸음수끼리 csv 병합
+        df1['heartrate'] = (df1['heartrate']/df1['steps'])  # 안정시 심박수 = 심박수/걸음수
         return df1
 
 
     def pre_processing(self, resting_heart_rate):
+        # 안정시 심박수데이터를 이동평균(moving averages) 400 
+        # 다운 샘플링을 1시간으로 하여 데이터를 평활화합니다.
 
         df_nonas = df1.dropna()
-        df1_rom = df_nonas.rolling(400).mean()
+        df1_rom = df_nonas.rolling(400).mean() #이동 평균
       
-        df2 = df1_rom.resample('10min').mean()
+        df2 = df1_rom.resample('10min').mean() #  다운 샘플링
         df2 = df2.dropna()
         return df2
 
-
+    # 주기성 보정 단계
     def seasonality_correction(self, heartrate, steps):
+        # 데이터를 사전 처리하고 계절성을 보정합니다.
 
+        # 심박수, 걸음수 데이터를 additive모델을 사용하여 trend + resid하여 나누고 주기를 1로 주어 분석을 합니다.
         sdHR_decomposition = seasonal_decompose(sdHR, model='additive', freq=1)
         sdSteps_decomposition = seasonal_decompose(sdSteps, model='additive', freq=1)
         sdHR_decomp = pd.DataFrame(sdHR_decomposition.resid + sdHR_decomposition.trend)
@@ -111,18 +118,20 @@ class HROSAD_offline:
         return data
 
 
+    # 표준화 단계
     def standardization(self, seasonality_corrected_data):
-
+        # 가우시안 분포 평균값이 0, 분산을 1로 단위분산(Z점수)으로 데이터를 표준화 합니다. 
         data_scaled = StandardScaler().fit_transform(data_seasnCorec.values)
         data_scaled_features = pd.DataFrame(data_scaled, index=data_seasnCorec.index, columns=data_seasnCorec.columns)
         data_df = pd.DataFrame(data_scaled_features)
         data = pd.DataFrame(data_df).fillna(0)
         data.to_csv('data.csv')
         return data
-
-
+    
+    # 모델 교육 및 이상탐지 예측
     def anomaly_detection(self, standardized_data):
-
+        # 표준화된 데이터를 사용하여 OC-SVM을 사용하여 특이치를 탐지 합니다.
+        # 분석된 데이터는 1은 정상 -1은 비정상으로 분류합니다.
         model =  EllipticEnvelope(contamination=outliers_fraction, 
                              #behaviour="new",
                              random_state=RANDOM_SEED, support_fraction=0.7)
@@ -138,8 +147,9 @@ class HROSAD_offline:
         data.to_csv('score.csv')
         return data
 
-
-    def visualize(self, results, symptom_date, diagnosis_date):
+    # 시각화
+    def visualize(self, results, symptom_date, diagnosis_date, user):
+        #결과를 csv파일, PNG파일로 저장
 
         try:
 
